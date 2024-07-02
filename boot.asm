5bcbc913cb62bbbbcca0d10ab2256013411fb281
aklimk <andrewklimko@outlook.com>
2024-07-02 19:05:34 +1000

    Added raw and padded options to bin creation script. Refactored code to work with globals, as its more space effecient. Added register stack saving and popping before all function calls to allow for better register utilization, reducing instructions. Reworked menu screen rendering to work correctly.

--- boot.asm
+++ boot.asm
@@ -3,304 +3,287 @@
 
 SCREEN_WIDTH equ 20
 SCREEN_HEIGHT equ 20
-GAMES_COUNT equ 3
+NUM_TITLES equ 3
+STACK_TOP equ 0xFFFF
+STACK_BOTTOM equ 0xAAAA
 
-; Assumed 0x7C00 - 0x7FFF
-; .text
-; .data
-; .bss
+; txt + data + bss : 0x7C00 (incl) - 0x7FFF (incl)
+; heap : 0x8000 (incl) upwards
+; stack : 0xFFFF (incl) downwards
+section .data
+	TITLE_ONE db "PONG", 0
+	TITLE_TWO db "DEMO-TITLE", 0
+	TITLE_THREE db "PLCEHLDR", 0
+	TITLES dw TITLE_ONE, TITLE_TWO, TITLE_THREE
+	TITLE_PADDINGS db 8, 8, 5, 5, 6, 6
 
-; Assumed 0x8000 - 0xFFFF
-; heap 0x8000 (incl) upwards
-; stack 0xFFFF (incl) downwards
+section .bss
+	SCREEN_STRING resb SCREEN_WIDTH * SCREEN_HEIGHT + (SCREEN_HEIGHT * 2)
+	SELECTION resb 1
+
 section .text
 main:
 	; Set up stack
-	mov di, 0xAAAA
+	mov di, STACK_BOTTOM
 	mov ss, di
-	mov sp, 0xFFFF
+	mov sp, STACK_TOP
 
-	; Padding Loop
-	; bx = i
-	mov bx, 0
-	start_padding_loop:
-	cmp bx, GAMES_COUNT
-	je end_padding_loop
-		; get string length of game text (ax)
-		; ax = strlen(GAMES[bx])
-		push bx
-		shl bx, 1	
-		mov di, [GAMES + bx]
-		call strlen
-		pop bx
-		
-		; calculate total padding (cx)
-		; cx = SCREEN_WIDTH - ax
-		mov cx, SCREEN_WIDTH
-		sub cx, ax
-		
-		; calculate left padding (GAME_PADDINGS[2*bx]), 
-		; GAME_PADDINGS[2*bx] = cx
-		mov si, bx
-		shl si, 1
-		add si, GAME_PADDINGS
-		mov [si], cx
-		shr byte [si], 1
-		
-		; calculate right padding (GAME_PADDINGS[2*bx + 1])
-		; GAME_PADDINGS[2*bx + 1] = cx ; 2 + ((cx / 2) % 2)
-		; cx
-		inc si
-		mov [si], cx
-		shr byte [si], 1
-		; ax = dividend
-		; bl = divisor
-		; al = quotient, ah = remainder
-		push bx
-		mov ax, [si]
-		mov bl, 2
-		div bl
-		; + (cx / 2) % 2)
-		add [si], ah
-		pop bx
-	inc bx
-	jmp start_padding_loop
-	end_padding_loop:
-
-	mov cx, 0 ; selection
-	mov dx, GAME_PADDINGS
-	call clear_screen
-	start_game_loop:
+	mov di, SCREEN_STRING
+	.start_game_loop:
+		call push_registers
 		call clear_screen
+		call pop_registers
+		
+		call push_registers
 		call fill_screen_string
-		mov di, SCREEN_STRING
+		call pop_registers
+		
+		call push_registers		
 		call print_string
-	jmp start_game_loop
-	stop_game_loop:
+		call pop_registers
+	jmp .start_game_loop
 
-; int strlen(string)
-; Get the length of a string 
-; without the null terminator.
-; Args:
-;     di: string to get the length of
+; Save all registers to the stack
+; (except bp)
+push_registers:
+	pop bp ; bp = return address
+	push ax
+	push bx
+	push cx
+	push dx
+	push si
+	push di
+	jmp bp
+
+; Load all registers from the stack
+; (except bp)
+pop_registers:
+	pop bp ; bp = return address
+	pop di
+	pop si
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	jmp bp
+
+; void print_string(char* string)
+; Prints the screen string to terminal.
+; Args: 
+;     di: string to print.
+;         Assumes its not an empty string.
 ; Returns:
-;     ax: length of string
-; Modifies: di, ax, bx
-strlen:
-	mov bx, 0
-	start_null_test_loop:
-	cmp byte [di + bx], 0
-	je end_null_test_loop
-		inc bx
-	jmp start_null_test_loop
-	end_null_test_loop:
-	mov ax, bx
+;     void
+print_string:
+	mov ah, 0x0E ; teletype
+	.print_char:
+		mov al, [di]
+		int 0x10 ; video services
+	inc di
+	cmp byte [di], 0x00
+	jne .print_char
 	ret
 
 ; void clear_screen()
 ; Clears the terminal screen.
 clear_screen:
-	mov ah, 0x07
-	mov al, 0x00
-	push bx
-	push cx
-	push dx
-		mov bh, 0x07
-		mov cx, 0x00
-		mov dx, 0x184F
-		int 0x10
-		mov ah, 0x02
-		mov bh, 0
-		mov dh, 0
-		mov dl, 0
-		int 0x10
-	pop dx
-	pop cx
-	pop bx
+	mov ax, 0x0700 ; scroll down window, clear
+	mov bx, 0x0700 ; white foreground, black background
+	mov cx, 0x0000 ; upper left position
+	mov dx, 0x184F ; lower right position
+	int 0x10 ; video services
+	mov ah, 0x02 ; set cursor position
+	mov bh, 0 ; page number
+	mov dx, 0 ; row, col
+	int 0x10 ; video services
 	ret
 
-; void print_string(char* string)
-; Prints the screen string to terminal.
-; Args: 
+; void fill_screen_border(char* screen_string)
+; Renders a border to screen_string.
+; Args:
 ;     none
-; Returns:
-;     void
-print_string:
-	mov bx, 0
-	mov ah, 0x0E ; function teletype of video services
-	print_char:
-		mov al, [SCREEN_STRING + bx]
-		int 0x10
-	inc bx
-	cmp byte [SCREEN_STRING + bx], 0
-	jne print_char
+; Returns: 
+;     none
+fill_screen_border:
+	mov di, SCREEN_STRING
+
+	mov ax, 0
+	.start_loop_y: 
+	cmp ax, SCREEN_HEIGHT
+	je .end_loop_y
+	; <<<for(int y = 0; y < SCREEN_HEIGHT;>>> y++)
+
+		mov bx, 0
+		.start_loop_x:
+		cmp bx, SCREEN_WIDTH
+		je .end_loop_x
+		; <<<for(int x = 0; x < SCREEN_WIDTH;>>> x++)
+			
+			mov cl, 0x20 ; Space
+			.if_top_boundry:
+			cmp ax, 0
+			jne .if_bottom_boundry
+			; if (y == 0)
+				mov cl, 0x23 ; hash
+			.if_bottom_boundry:
+			cmp ax, SCREEN_HEIGHT - 1
+			jne .if_left_boundry
+			; if (y == SCREEN_HEIGHT - 1)
+				mov cl, 0x23 ; hash
+			.if_left_boundry:
+			cmp bx, 0
+			jne .if_right_boundry
+			; if (x == 0)
+				mov cl, 0x23 ; hash
+			.if_right_boundry:
+			cmp bx, SCREEN_WIDTH - 1
+			jne .endif_boundry
+			; if (x == SCREEN_WIDTH - 1)
+				mov cl, 0x23 ; hash
+			.endif_boundry:
+			
+			; set char
+			mov [di], cl
+			inc di
+	
+		; for(int x = 0; x < SCREEN_WIDTH; <<<x++)>>>
+		inc bx
+		jmp .start_loop_x
+		.end_loop_x:
+
+		; x == SCREEN_WIDTH
+		; add \r\n to screen_string
+		mov byte [di], 0x0A
+		inc di
+		mov byte [di], 0x0D
+		inc di
+
+	; for(int y = 0; y < SCREEN_HEIGHT; <<<y++)>>>
+	inc ax
+	jmp .start_loop_y
+	.end_loop_y:
+
+	; y == SCREEN_HEIGHT
 	ret
+
 
 ; void fill_screen_string(char* screen_string, int selection, int* paddings)
 ; Renders the current menu state to screen_string.
-; Args:
-; Returns: void
+; Args: 
+;     none
+; Returns:
+;	  none
 fill_screen_string:
-	push ax
-	push bx
-	push cx
-	push si
+	; create border around screen
+	call push_registers
 	call fill_screen_border
-	pop si
-	pop cx
-	pop bx
-	pop ax
+	call pop_registers
+
+	mov di, SCREEN_STRING
+	mov si, TITLE_PADDINGS
 
 	mov ax, 0
-	mov di, GAME_PADDINGS
-	mov si, SCREEN_STRING
-	loop_y_start_2:
+	.start_loop_y:
 	cmp ax, SCREEN_HEIGHT
-	je loop_y_end_2
+	je .end_loop_y
+	; <<<for(int y = 0; y < SCREEN_HEIGHT;>>> y++)
+
 		mov bx, 0
-		loop_x_start_2:
+		.start_loop_x:
 		cmp bx, SCREEN_WIDTH
-		je loop_x_end_2
-			; > rendering
-			if_selector_x:
-			push dx
-			mov dx, cx
-			add dx, 2
-			cmp ax, dx
-			jne endif_selector
-				if_selector_y:
+		je .end_loop_x
+		; <<<for(int x = 0; x < SCREEN_WIDTH;>>> x++)
+
+			; char ">" rendering
+			.if_selector_x:
+			mov cx, [SELECTION]
+			add cx, 2
+			cmp ax, cx
+			jne .endif_selector
+			; if (y == SELECTION + 2)
+				.if_selector_y:
 				cmp bx, 2
-				jne endif_selector
-					mov byte [si], 0x3E ; >
-			endif_selector:
-			pop dx
+				jne .endif_selector
+				; if (x == 2)
+					mov byte [di], 0x3E ; >
+			.endif_selector:
 			
-			; text rendering
-			if_text_y_1:
+			; title rendering
+			.if_text_y_gt:
 			cmp ax, 1
-			jle end_text_if
-				if_text_y_2:
-				cmp ax, GAMES_COUNT + 1
-				jg end_text_if
-					if_text_x_1:
-					cmp bl, [di]
-					jb end_text_if
-						if_text_x_2:
-						mov dx, SCREEN_WIDTH
-						sub dx, [di + 1]
-						push ax
-						mov ax, dx
-						cmp bl, al
-						pop ax
-						jae end_text_if
-							; render text
-							push ax
-							sub al, 2 ; dx = y - 2
-							mov ah, 0
-							mov bp, ax
-							pop ax
+			jle .end_text_if
+			; if (y > 1)
+				.if_text_y_le:
+				cmp ax, NUM_TITLES + 1
+				jg .end_text_if
+				; if (y <= NUM_TITLES + 1)
+					.if_text_x_gt:
+					cmp bl, [si]
+					jb .end_text_if
+					; if (x > paddings[0])
+						.if_text_x_2:
+						mov cx, SCREEN_WIDTH
+						sub cl, [si + 1]
+						cmp bl, cl
+						jae .end_text_if
+						; if (x <= SCREEN_WIDTH - paddings[1])
+							; Render Text
+							push di
+							; GAMES[y - 2]
+							mov di, ax 
+							sub di, 2 ; bp = y - 2
+							; GAMES elem size is 16 bits, thus
+							; bp = 2(y - 2) to index correctly
+							shl di, 1 
+							; bp = char*
+							mov di, [TITLES + di] ; bp = GAMES[y - 2]
 						
-							mov dx, [GAMES] ; dx = GAMES[y - 2]
-							push ax
-							mov bp, dx
-							mov al, [bp]
-							mov [si], al
-							mov bp, [GAMES]
-							mov al, [bp]
-							mov [si], al
-							pop ax
-
-							push ax
-							mov ax, bx ; ax = x, bx = x
-							sub bl, [di] ; bx = x - paddings[0]
-							add bx, dx ; bx = GAMES[y - 2] + (x - paddings[0])
-							mov bl, [bx] ; bx = GAMES[y - 2][x - paddings[0]]
-							mov bx, ax ; restore bx to x
-							pop ax
-			end_text_if:
-			inc si
-		inc bx
-		jmp loop_x_start_2
-		loop_x_end_2:
-		
-		; should increment padding array?
-		if_text_y_1_2:
-			cmp ax, 1
-			jle endif_text_2
-				if_text_y_2_2:
-				cmp ax, GAMES_COUNT + 1
-				jg endif_text_2
-					add di, 2
-		endif_text_2:
-		
-		; \r\n
-		add si, 2
-	inc ax
-	jmp loop_y_start_2
-	loop_y_end_2:
-	ret
+							; GAMES[y - 2][x - paddings[0]]
+							mov cx, bx ; cx = x
+							sub cl, [si] ; cx = x - paddings[0]
+							add di, cx ; bp = GAMES[y - 2] + (x - paddings[0])
 	
+							; derefernce using si instead of bp, as bp uses
+							; ss instead of ds 
+							mov cl, [di] ; cl = GAMES[y - 2][x - paddings[0]]
+							pop di	
 
-; void fill_screen_border(char* screen_string)
-; Renders a border in screen_string.
-; Args:
-;     di: char array to create a border in
-; Returns: void
-fill_screen_border:
-	mov ax, 0
-	mov si, 0
-	loop_y_start:
-	cmp ax, SCREEN_HEIGHT
-	je loop_y_end
-		mov bx, 0
-		loop_x_start:
-		cmp bx, SCREEN_WIDTH
-		je loop_x_end
-			mov cx, 0x20 ; Space
-			if_top_boundry:
-			cmp ax, 0
-			jne if_bottom_boundry
-				mov cx, 0x23 ; hash
-			if_bottom_boundry:
-			cmp ax, SCREEN_HEIGHT - 1
-			jne if_left_boundry
-				mov cx, 0x23 ; hash
-			if_left_boundry:
-			cmp bx, 0
-			jne if_right_boundry
-				mov cx, 0x23 ; hash
-			if_right_boundry:
-			cmp bx, SCREEN_WIDTH - 1
-			jne end_boundry_if
-				mov cx, 0x23 ; hash
-			end_boundry_if:
-			push bx	
-			mov bx, di
-			mov [bx + si], cx
-			pop bx
-			inc si
+							; char at (x, y) = GAMES[y - 2][x - paddings[0]]
+							mov [di], cl
+			.end_text_if:
+			
+			; next char
+			inc di
+
+		; for(int x = 0; x < SCREEN_WIDTH; <<<x++)>>>
 		inc bx
-		jmp loop_x_start
-		loop_x_end:
-		push bx
-		mov bx, di
-		mov byte [bx + si], 0x0A
-		inc si
-		mov byte [bx + si], 0x0D
-		pop bx
-		inc si
+		jmp .start_loop_x
+		.end_loop_x:
+
+		; x == SCREEN_WIDTH	
+
+		; compensate for \r\n
+		add di, 2
+		
+		; increment padding array to next padding pair
+		; iff currently on a title row
+		.if_text_y_gt_2:
+			cmp ax, 1
+			jle .endif_text_2
+			; y > 1
+				.if_text_y_lt_2:
+				cmp ax, NUM_TITLES + 1
+				jg .endif_text_2
+				; y < GAMES_COUNT
+					add si, 2
+		.endif_text_2:
+		
+
+	; for(int y = 0; y < SCREEN_HEIGHT; <<<y++)>>>
 	inc ax
-	jmp loop_y_start
-	loop_y_end:
+	jmp .start_loop_y
+	.end_loop_y:
+
+	; y == SCREEN_HEIGHT
 	ret
 
-section .data
-	GAME_ONE db "PONG", 0
-	GAME_TWO db "DEMO", 0
-	GAME_THREE db "PLACEHOLDER", 0
-	GAMES dw GAME_ONE, GAME_TWO, GAME_THREE
-
-section .bss
-	SCREEN_STRING resb SCREEN_WIDTH * SCREEN_HEIGHT + (SCREEN_HEIGHT * 2)
-	GAME_PADDINGS resb GAMES_COUNT * 2
-	SELECTION resb 1
