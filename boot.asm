cd472cddfbd70bebd83206d15ce78e3ad178b67a
aklimk <andrewklimko@outlook.com>
2024-07-06 01:31:01 +1000

    Added size and binary assembled result comments next to instruction to aid in finding optimization oppurtunities. Done for the menu text rendering loop and introductory code.

--- boot.asm
+++ boot.asm
@@ -33,111 +33,145 @@ section .bss
 	BALL_Y resb 1
 	BALL_VELOCITY resb 1 
 
+
+; Common Instructions Reference
+; push + pop = 4B
+; logical r16, r16 = 2B
+
+; cmp al, im8 = 2B
+; cmp ax, im16 = 3B
+; cmp r8, im8 = 3B
+; cmp r16, im16 = 4B
+
+; call (near) = 3B
+; call (far) = 5B
+; ret = 1B
+
+; jump (relative) = 2B
+; jump (near) = 3B
+; jump (far) = 5B
+
+; mov r8, r8 or r16, r16 = 2B
+; mov r8, im8 = 2B
+; move r16, im16 = 3B
+; move m8, r8 or r8, m8 = 4B (3B if add in reg)
+; move m16, r16, or r16, m16 =  4B (3B if add in reg)
 section .text
 setup_stack:
-	mov di, STACK_BOTTOM
-	mov ss, di
-	mov sp, STACK_SIZE
+	mov ax, STACK_BOTTOM ; B8 00 80 (1op + 2im)
+	mov ss, ax ; 8E D0 (1op + 1r)
+	mov sp, STACK_SIZE ; BC AA AA (1op + 2im)
+	; - SECTION 8B
 
 main:
-	; reset to menu
-	mov ax, 0x03
-	int 0x10
+	; Reset to Text mode
+	mov ax, 0x0003 ; B8 03 00 (1op + 2im)
+	int 0x10 ; CD 10 (1op + 1im)
+	; - SECTION 5B
 
-	; menu loop
+	; Menu Loop
 	.start_game_loop:
 		; Clear Menu
-		call clear_screen
+		call NEAR clear_screen ; E8 D6 00 (1op + 2near-add)
 		
 		; Menu Rendering Start
-		mov di, SCREEN_STRING
-		mov si, TITLE_PADDINGS
+		mov di, SCREEN_STRING ; BF D8 7D (1op + 2ptr)
+		mov si, TITLE_PADDINGS ; BE C0 7D (1op + 2ptr)
+	; - SECTION 9B
 
-		mov ax, 0
+		mov ax, 0 ; B8 00 00 (1op + 2im)
 		.loop_y:
-			mov bx, 0
+			mov bx, 0 ; BB 00 00 (1op + 2im)
 			.loop_x:
+
+				; cl = space
+				mov cl, 0x20 ; B1 20 (1op + 1im)
+	; - SECTION 8B
+
 				; Create Border
-				mov cl, 0x20 ; Space
 				.if_top_boundry:
-				cmp al, 0
-				jne SHORT .if_bottom_boundry
+				cmp al, 0 ; 3C 00 (1op + 1im)
+				jne SHORT .if_bottom_boundry ; 75 02 (1op + 1off)
 				; if (y == 0)
-					mov cl, 0x23 ; hash
+					; cl = hash
+					mov cl, 0x23 ; B1 23 (1op + 1im)
 				.if_bottom_boundry:
-				cmp al, SCREEN_HEIGHT - 1
-				jne SHORT .if_left_boundry
+				cmp al, SCREEN_HEIGHT - 1 ; 3C 13 (1op + 1im)
+				jne SHORT .if_left_boundry ; 75 02 (1op + 1off)
 				; if (y == SCREEN_HEIGHT - 1)
-					mov cl, 0x23 ; hash
+					mov cl, 0x23 ; B1 23 (1op + 1im)
 				.if_left_boundry:
-				cmp bl, 0
-				jne SHORT .if_right_boundry
+				cmp bl, 0 ; 80 FB 00 (1op + 1reg + 1im)
+				jne SHORT .if_right_boundry ; 75 02 (1op + 1off)
 				; if (x == 0)
-					mov cl, 0x23 ; hash
+					mov cl, 0x23 ; B1 23 (1op + 1im)
 				.if_right_boundry:
-				cmp bl, SCREEN_WIDTH - 1
-				jne SHORT .endif_boundry
+				cmp bl, SCREEN_WIDTH - 1 ; 80 FB 13 (1op + 1reg + 1im)
+				jne SHORT .endif_boundry ; 75 02 (1op + 1off)
 				; if (x == SCREEN_WIDTH - 1)
-					mov cl, 0x23 ; hash
+					mov cl, 0x23 ; B1 23 (1op + 1im)
 				.endif_boundry:
-				
+	; - SECTION	24B
 		
-				; Menu Rendering
-				; char ">" rendering
+				; ">" Char Rendering
 				.if_selector_x:
-				mov dl, [SELECTION]
-				add dl, 2
-				cmp al, dl
-				jne SHORT .endif_selector
+				mov dl, [SELECTION] ; 8A 16 90 7F (1op + 1reg + 2add)
+				add dl, 2 ; 80 C2 02 (1op + 1reg + 1im)
+				cmp al, dl ; 38 D0 (1op + 1(reg + reg))
+				jne SHORT .endif_selector ; 75 07 (1op + 1off)
 				; if (y == SELECTION + 2)
 					.if_selector_y:
-					cmp bl, 2
-					jne SHORT .endif_selector
+					cmp bl, 2 ; 80 FB 02 (1op + 1reg + 1im)
+					jne SHORT .endif_selector ; 75 02 (1op + 1off)
 					; if (x == 2)
-						mov cl, 0x3E ; >
+						; cl = >
+						mov cl, 0x3E ; B1 3E (1op 1im)
 				.endif_selector:
+	; - SECTION 18B
 				
-				; title rendering
+				; Game Titles Rendering
 				.if_text_y_gt:
-				cmp al, 1
-				jle SHORT .end_text_if
+				cmp al, 1 ; 3C 01 (1op + 1im)
+				jle SHORT .end_text_if ; 7E 26 (1op + 1off)
 				; if (y > 1)
 					.if_text_y_le:
-					cmp al, NUM_TITLES + 1
-					jg SHORT .end_text_if
+					cmp al, NUM_TITLES + 1 ; 3C 04 (1op + 1im)
+					jg SHORT .end_text_if ; 7F 22 (1op + 1off)
 					; if (y <= NUM_TITLES + 1)
 						.if_text_x_gt:
-						cmp bl, [si]
-						jb SHORT .end_text_if
+						cmp bl, [si] ; 3A 1C (1op + 1(reg + regmem))
+						jb SHORT .end_text_if ; 72 1E (1op + 1off) 
 						; if (x > paddings[0])
 							.if_text_x_2:
-							mov dl, SCREEN_WIDTH
-							sub dl, [si + 1]
-							cmp bl, dl
-							jae SHORT .end_text_if
+							mov dl, SCREEN_WIDTH ; B2 14 (1op + 1off)
+							sub dl, [si + 1] ; 2A 54 01 (1op + 1regmem + 1im)
+							cmp bl, dl ; 38 D3 (1 op + 1(reg + reg))
+							jae SHORT .end_text_if ; 73 15 (1op + 1off)
 							; if (x <= SCREEN_WIDTH - paddings[1])
+	; - SECTION 21B
 								; Render Text
-								push di
+								push di ; 57 (1(op + reg))
+
 								; GAMES[y - 2]
-								mov di, ax 
-								sub di, 2 ; bp = y - 2
+								mov di, ax ; 89 C7 (1op + 1(reg + reg))
+								sub di, 2 ; 83 EF 02 (1op 1reg 1im)
 								; GAMES elem size is 16 bits, thus
 								; bp = 2(y - 2) to index correctly
-								shl di, 1 
+								shl di, 1 ; D1 E7 (1op + 1reg)
+								mov di, [TITLES + di] ; 8B BD BA 7D
 								; bp = char*
-								mov di, [TITLES + di] ; bp = GAMES[y - 2]
 							
-								; GAMES[y - 2][x - paddings[0]]
-								mov dl, bl ; dx = x
-								sub dl, [si] ; dx = x - paddings[0]
-								add di, dx ; bp = GAMES[y - 2] + (x - paddings[0])
+								; &GAMES[y - 2][x - paddings[0]]
+								mov dl, bl ; 88 DA (1op + 1(reg + reg))
+								sub dl, [si] ; 2A 14 (1op + 1regmem)
+								add di, dx ; 01 D7 (1op + 1(reg + reg))
 		
-								; derefernce using si instead of bp, as bp uses
-								; ss instead of ds 
-								mov cl, [di] ; cl = GAMES[y - 2][x - paddings[0]]
-								pop di	
+								mov cl, [di] ; 8A 0D (1op + 1(reg + regmem))
+
+								pop di ; 5F (1(op + reg)
 				.end_text_if:
-				
+	; - SECTION 21B				
+
 				; set char
 				mov [di], cl
 				inc di
@@ -225,18 +259,19 @@ main:
 
 	jmp NEAR .start_game_loop
 
+
 ; void print_string(char* string)
 ; Prints the screen string to terminal.
 ; Args: 
-;     di: string to print.
+;     string (di): string to print.
 ;         Assumes its not an empty string.
-; Returns:
-;     void
 print_string:
-	mov ah, 0x0E ; teletype
+	; Teletype
+	mov ah, 0x0E 
 	.print_char:
 		mov al, [di]
-		int 0x10 ; video services
+		; Video Interupt
+		int 0x10 
 	inc di
 	cmp byte [di], 0x00
 	jne SHORT .print_char
