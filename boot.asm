1a2af4b0d907352de8d54d896d3ab7b1f2c86e7e
Andrew Klimenko <andrewklimko@outlook.com>
2025-08-11 00:09:43 +1000

    Version without menu. Added a WIP score menu to the beginning instead.

--- boot.asm
+++ boot.asm
@@ -8,41 +8,43 @@ STACK_BOTTOM equ 0x8000
 
 SCREEN_WIDTH equ 320
 SCREEN_HEIGHT equ 200
-BALL_DIM equ 50
+RANGE equ (SCREEN_HEIGHT * SCREEN_WIDTH) / 2
+
+BALL_DIM equ 5
 BALL_START_X equ (SCREEN_WIDTH - BALL_DIM) / 2
 BALL_START_Y equ (SCREEN_HEIGHT - BALL_DIM) / 2
 PLAYER_PADDLE_OFFSET equ 20
-PLAYER_PADDLE_WIDTH equ 10
+ENEMY_PADDLE_OFFSET equ 319 - PLAYER_PADDLE_OFFSET - PLAYER_PADDLE_WIDTH
+PLAYER_PADDLE_WIDTH equ 5
 PLAYER_PADDLE_HEIGHT equ 50
 
-section .bss
+
+
+; ~~~~~ CODE SECTION ~~~~~
 
 section .text
 
-.test:
-	jmp SHORT .test	
 
-main:
-	; Set up video and stack settings.
-
-	; 320x200 8 bit color graphical mode.
-	mov al, 0x13
-	int 0x10
-
-	; Memory adressing is done using a segment and offset.
-	; Video memory starts at segment 0xA000.
-	; Video memory is 1 byte per pixel, column major, index from top left.
-	; Ds does not support direct mov, use a general register first.
-	mov ax, 0xA000
-	mov ds, ax
-
+; ~~~~~ SETUP STACK SPACE ~~~~~
+setup_stack:
 	; Set up stack space by setting stack segment register and stack pointer register.
 	mov ax, STACK_BOTTOM
 	mov ss, ax
 	mov sp, STACK_SIZE
 
-	
-	; Map variables to available registers.
+
+main:
+	; ~~~~~ SETUP VIDEO ~~~~~
+	; 320x200 8 bit color graphical mode.
+	; Memory adressing is done using a segment and offset.
+	; Video memory starts at segment 0xA000.
+	; Video memory is 1 byte per pixel, column major, index from top left.
+	mov ax, 0x13
+	int 0x10
+
+
+
+	; ~~~~~ 16 BIT REGISTERS AND GAME VARIABLE MAP ~~~~~
 
 	; Registers
 	; ax, bx, cx, dx - general purpose, 16 bit or byte adressable, supports immediate mov
@@ -58,29 +60,220 @@ main:
 	; Ball position y : di 
 	; Left paddle score : cl
 	; Right paddle score : ch
-	; Leaves dl, dh, bp free
-	
-	; Paddles start at the top.
-	xor ax, ax ; one byte register clear
+	; Leaves dx (dl, dh), bp free
 
-	; Ball velocity starst at 1.
-	mov bl, 1
-	mov bh, 1
-	
+
+
+	; ~~~~~ INITILIZE GAME VARIABLES ~~~~~
+
+	; ~~~~~ INITILIZE PADDLES ~~~~~
+	; Both paddles start at 1, at the top, just after the border.
+	mov ax, 0x0101
+
+	; ~~~~~ INITILIZE BALL VELOCITY ~~~~~~
+	; Use the gen rand velocity function to generate a random velocity for both ball x and y.
+	; gen_rand_velocity generates to the dl register.
+	call gen_rand_velocity
+	mov bl, dl
+	call gen_rand_velocity
+	mov bh, dl
+
+	; ~~~~~ INITILIZE BALL POSITION ~~~~~	
 	; Ball position starts at the center.
 	mov si, BALL_START_X
 	mov di, BALL_START_Y
 
-	; Paddle scores start at 0.
-	xor cx, cx
+	; ~~~~~ INITILIZE PADDLE SCORES ~~~~~
+	xor cx, cx ; one byte register clear
 
 
-	; Main loop for the game.
 
+	; ~~~~~ MAIN GAME LOOP ~~~~~
 	.game_loop:
-		; Render left paddle
-		pusha
+		; ~~~~~ BEEPING TEST ~~~~~
+		; Beep test.
+		; push ax
+		; mov ah, 0x0E
+		; mov al, 7
+		; int 0x10
+		; pop ax
 
+
+
+		; ~~~~~ GAME INPUT LOGIC ~~~~~
+		; Start paddle input, use non-blocking poll of keyboard presses.
+		; ah = key scan code, zf = 0 if key presed. al = ascii char, ax = 0 if no key pressed.
+		push ax
+		mov ah, 0x01
+		int 0x16
+
+		jnz SHORT .pop_endif
+			pop ax 
+		.pop_endif:
+
+		jz SHORT .endif
+			; If key detected, redo with blocking input. As non-blocking seems to miss keys.
+			xor ah, ah
+			int 0x16
+			; Move scan code to dh, so ax can be popped from the stack.
+			mov dh, ah
+			pop ax
+
+			; Test if 0x48 (up) scancode detected.
+			cmp dh, 0x48
+			jne SHORT .if_down
+				; Block movment if paddle is too high.
+				cmp al, 4
+				jbe SHORT .if_down
+					sub al, 4
+			; Test if 0x50 (down) scancode detected.
+			.if_down:
+			cmp dh, 0x50
+			jne SHORT .endif
+				; Block movement if paddle is too low.
+				cmp al, 199 - PLAYER_PADDLE_HEIGHT - 4
+				jae SHORT .endif
+					add al, 4
+		.endif:
+
+
+		
+		; ~~~~~ GAME PHYSICS LOGIC ~~~~~
+
+		; ~~~~~ BALL POSITION UPDATE ~~~~~
+		; Need to upgrade 8 bit register to 16 bit in order to add together.
+		; movsx is a sign-preserving mov.
+		; X component.
+		xor dx, dx
+		movsx dx, bl
+		add si, dx
+		; Y component.
+		xor dx, dx 
+		movsx dx, bh
+		add di, dx
+
+		; ~~~~~ BALL COLLISION DETECTION ~~~~~
+
+		; ~~~~~ PLAYER SCORING ZONE ~~~~~
+		; Detect if the right side of the ball is to the left of the left edge of the left paddle.
+		; This indicates the enemy has scored.
+		cmp si, PLAYER_PADDLE_OFFSET - BALL_DIM
+		jae .end_if_left_wall
+			; Enemy has scored.
+			inc ch
+			jmp show_score
+		.end_if_left_wall:
+
+		; ~~~~~ ENEMY SCORING ZONE ~~~~~
+		; Detect if the left side of the ball is to the right of the right edge of the right paddle.
+		cmp si, 319 - PLAYER_PADDLE_OFFSET - PLAYER_PADDLE_WIDTH
+		jbe .end_if_right_wall
+			; Player has scored.
+			inc cl
+			jmp show_score
+		.end_if_right_wall:
+
+		; ~~~~~ LEFT PADDLE ~~~~~
+		call paddle_hit_detection
+
+		; ~~~~~ RIGHT PADDLE ~~~~~
+		call paddle_hit_detection
+
+		; ~~~~~ ROOF ~~~~~
+		cmp di, 1
+		jae .end_if_roof
+			; Flip y velocity.
+			neg bh
+		.end_if_roof:
+
+		; ~~~~~ FLOOR ~~~~~
+		cmp di, 199 - BALL_DIM
+		jbe .end_if_floor
+			; Flip y velocity.
+			neg bh
+		.end_if_floor:
+
+
+
+		; ~~~~~ AI PLAYER MOVEMENT ~~~~~
+		; If ball is higher than paddle, go up. Otherwise go down.
+		xor dx, dx
+		mov dl, ah
+		add dx, PLAYER_PADDLE_HEIGHT / 2
+		; Jump if paddle has a lower or equal value (is higher up) than the ball.
+		cmp dx, di
+		jbe .endif_move_up	
+			; Jump if enemy paddle is at 1 (can't move up).
+			cmp ah, 1
+			jbe .endif_move_up
+				dec ah
+		.endif_move_up:
+		xor dx, dx
+		mov dl, ah
+		add dx, PLAYER_PADDLE_HEIGHT / 2
+		; Jump if paddle has a higher or equal value (is lower down) than the ball.
+		cmp dx, di
+		jae .endif_move_down
+			; Jump if enemy paddle is at 197 (Can't move down).
+			cmp ah, 198 - PLAYER_PADDLE_HEIGHT
+			jae .endif_move_down
+				inc ah	
+		.endif_move_down:
+
+
+		
+		; ~~~~~ RENDERING LOGIC ~~~~~
+		; Ds does not support direct mov, use a general register first.
+		push ax
+		mov ax, 0x4000
+		mov es, ax
+		pop ax
+
+		; Clear screen.
+		pusha
+		cld
+		xor ax, ax
+		xor di, di
+		mov cx, RANGE
+		rep stosw
+		popa
+
+		; Render border.
+		; Left line.
+		pusha
+		xor ax, ax
+		xor bx, bx
+		xor cx, cx
+		mov dx, 199
+		call render_rectangle
+		popa
+		; Top line.
+		pusha
+		xor ax, ax
+		xor bx, bx
+		mov cx, 319
+		xor dx, dx
+		call render_rectangle
+		popa
+		; Right line.
+		pusha
+		mov ax, 319
+		xor bx, bx
+		mov cx, 319
+		mov dx, 199
+		call render_rectangle
+		popa
+		; Bottom line.
+		pusha
+		xor ax, ax
+		mov bx, 199
+		mov cx, 319
+		mov dx, 199
+		call render_rectangle
+		popa
+
+		; Render left paddle.
+		pusha
 		; Move left paddle vertical position to bx register.
 		xor bx, bx
 		mov bl, al
@@ -94,42 +287,245 @@ main:
 		add dx, PLAYER_PADDLE_HEIGHT
 
 		call render_rectangle
+		popa
 
+		; Render right paddle.
+		pusha
+		; Move right paddle vertical position to bx register.
+		xor bx, bx
+		mov bl, ah
+		; Right paddle horizontal position is constant.
+		mov ax, ENEMY_PADDLE_OFFSET
+
+		; Calculate bottom right position using preset width, height values.
+		mov cx, ax
+		add cx, PLAYER_PADDLE_WIDTH
+		mov dx, bx
+		add dx, PLAYER_PADDLE_HEIGHT
+
+		call render_rectangle
+		popa
+
+		; Render ball.
+		pusha
+		; Ball x and y position.
+		mov ax, si
+		mov bx, di
+
+		; Calculate bottom right position using BALL_DIM.
+		mov cx, ax
+		add cx, BALL_DIM
+		mov dx, bx
+		add dx, BALL_DIM
+
+		call render_rectangle
+		popa
+
+		; Copy render over to actuall video memory area. (flip).
+		pusha
+		cld
+		; Set ds to source memory area
+		push es
+		pop ds 
+		; Set es to destination memory area
+		mov ax, 0xA000
+		mov es, ax
+
+		; Clear indexes and copy memory over.
+		xor si, si
+		xor di, di
+		mov cx, RANGE
+		rep movsw
 		popa
 		
-		jmp SHORT .game_loop
+		; Limit fps to 60.
+		pusha
+		mov  cx, 0           ; CX:DX = microseconds to wait
+   		mov  dx, 16667  
+    	mov  ah, 86h
+    	int  15h             ; waits with interrupts enabled
+		popa
+
+		jmp .game_loop
 	 
 	
 
-; ax = topleft x (also x counter)
-; bx = topleft y (also y counter)
+; ax = topleft x 
+; also modifies di (x counter)
+; bx = topleft y (also is the y counter)
 ; cx = bottomright x
 ; dx = bottomright y
 ; also modifies si
-; also modifies di
 render_rectangle:
 	.loop_y:
 	; Jump to end of loop if topleft y > bottomright y.
 	cmp bx, dx
 	ja SHORT .end_loop_y
+		mov di, ax
 		.loop_x:
 		; Jump to end if topleft x > bottomright x.
-		cmp ax, cx
+		cmp di, cx
 		ja SHORT .end_loop_x
 
 			; Main rendering logic for rectangle.		
-			; Render a white pixel at screen position (ax, bx).
+			; Render a white pixel at screen position (di, bx).
 			; Si holds the unrolled position.
 			mov si, bx
-			imul si, SCREEN_HEIGHT
-			add si, ax
-			mov BYTE [ds:si], 255
+			imul si, SCREEN_WIDTH
+			add si, di
+			mov BYTE [es:si], 0x0F
 
 			; Increment the x counter.
-			inc ax
+			inc di
+			jmp SHORT .loop_x
 		.end_loop_x:
 		
 		; Increment the y counter.
 		inc bx
+		jmp SHORT .loop_y
 	.end_loop_y:
 	ret
+
+
+
+; Ball velocity starts at a random number from -3 - 3 (not 0) for both x and y.
+; Generates to  register.
+gen_rand_velocity:
+	push ax
+	push bx
+
+	; Read byte from timer, psuedo-random byte.
+	in al, 0x40
+
+	; Multiply al by 6 and put it into ax.
+	; overflow causes ah to be in the range 0-5.	
+	mov bl, 6
+	mul bl
+
+	; Shift the range 0-5 to -3 to 2. 
+	sub ah, 3
+
+	; Flip the cf flag and add it back to ah.
+	; cf is 1 for positive results (0, 1, 2),
+	; range becomes -3 - -1 and 1 - 3 .
+	cmc
+	adc ah, 0
+
+	; save result to dl
+	mov dl, ah
+
+	pop bx
+	pop ax
+
+	ret
+
+
+
+; General hit detection for the vertical portion of a paddle.
+; Paddle y position should be stored in bp register as input.
+; No output.
+paddle_vertical_hit_detection:
+	; Check if the ball is below the top edge of the paddle.
+	xor dx, dx
+	mov dl, bp
+	sub dx, BALL_DIM
+	cmp di, dx
+	jbe .endif_paddle_y1
+		; Check if the ball is above the bottom edge of the paddle.
+		xor dx, dx
+		mov dl, bp
+		add dx, PLAYER_PADDLE_HEIGHT
+		cmp di, dx
+		jae .endif_paddle_y2
+			; Ball is hitting the paddle. 
+			; Invert y velocity then
+			; Increment both vertical and horizontal velocity 
+			; in whatever direction its heading.
+			or bh, bh
+			jns SHORT .endif_left_dec1
+				dec bh
+			.endif_left_dec1:
+			inc bh
+
+			neg bl
+			or bl, bl
+			jns SHORT .endif_left_dec2
+				dec bl
+			.endif_left_dec2:
+			inc bl
+		.endif_paddle_y2:
+	.endif_paddle_y1:
+
+
+
+show_score:
+	; Show current score
+	pusha 
+
+	; Cheap clear screen
+	mov ax, 0x13
+	int 0x10
+
+	; Move cursor for left paddle score.
+	; Page 0.
+	xor bh, bh
+	; Bright white palette
+	mov bl, 15
+	; DH = row, DL = col
+	mov dx, (10 << 8) | 10
+	; Move cursor opcode.
+	mov ah, 0x02
+	int 0x10
+
+	; Print left paddle score.
+	xor ax, ax
+	mov al, cl
+	aam
+	xchg al, ah
+	add ax, 0x3030
+
+	mov dl, ah
+	mov ah, 0x0E
+	int 0x10
+	mov al, dl
+	mov ah, 0x0E
+	int 0x10
+
+	; Move cursor for right paddle score.
+	; Page 0.
+	xor bh, bh
+	; Bright white palette
+	mov bl, 15
+	; DH = row, DL = col
+	mov dx, (10 << 8) | 30
+	; Move cursor opcode.
+	mov ah, 0x02
+	int 0x10
+
+	; Print right paddle score.
+	xor ax, ax
+	mov al, ch
+	aam
+	xchg al, ah
+	add ax, 0x3030
+
+	mov dl, ah
+	mov ah, 0x0E
+	int 0x10
+	mov al, dl
+	mov ah, 0x0E
+	int 0x10
+
+	; Give screen 3 seconds.
+	mov cx, 3000000 >> 16 
+	mov dx, 0xFFFF
+	mov ah, 0x86
+	int 0x15
+
+	; Cheap clear screen
+	mov ax, 0x13
+	int 0x10
+
+	popa
+
+	jmp main
